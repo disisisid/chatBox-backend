ae7321a02408bfe7d556b48bce2f034742999d61
disisisid <sidnshady@gmail.com>
2020-03-29 20:20:17 +0530

    addded endpoing that returns port to the client

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const io = require('socket.io')(3000)
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 4000
+
+app.use(cors())
+
+app.get('/get-port', (req, res) => {
+  console.log('port requested');
+  res.json({port: process.env.port || '3000'})
+})
 
 io.on('connection', socket => {
 //  socket.emit('chat-message', 'hello');
-  console.log('new user');
+  console.log('new user connected');
   socket.on('send-chat-message', message => {
     console.log(message);
     socket.broadcast.emit('chat-message', message);
   })
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  })
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
